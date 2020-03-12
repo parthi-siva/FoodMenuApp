49aa68339749f13d11a4c30b46731ea95cba33f3
Parthiban <parthimyself90@gmail.com>
2020-03-12 20:19:22 +0530

    Added login logic

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,15 @@
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from app import app
+from app.forms import LoginForm
 
-@app.route("/")
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
+@app.route('/index')
 def index():
-    return render_template('login/login.html')
+    return render_template('base.html')
